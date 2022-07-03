9eb6589bfa0e4edbd1f7de56b7cee0972a5127fc
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-07-03 03:33:36 +0100

    DockingManager: Add feature to charge connected ships when two ships are docked together.

--- DockingManager/Program.cs
+++ DockingManager/Program.cs
@@ -31,7 +31,126 @@ namespace DockingManager
         private const string IniToggleGyroscopes = "Toggle Gyroscopes";
         private const string IniMaintainPower = "MaintainPower";
 
-        private readonly MyIni _ini = new MyIni();
+        private const string IniRechargeBatteriesOfConnectedShipsIfOverPercent =
+            "RechargeBatteriesOfConnectedShipsIfOverPercent";
+
+        private const string IniRefillHydrogenOfConnectedShipsIfOverPercent =
+            "RefillHydrogenOfConnectedShipsIfOverPercent";
+
+        private class Ini
+        {
+            private static readonly MyIni Parser = new MyIni();
+
+            private static readonly IDictionary<IMyProgrammableBlock, string> IniTextCache =
+                new Dictionary<IMyProgrammableBlock, string>();
+
+            private static readonly IDictionary<IMyProgrammableBlock, Ini> IniCache =
+                new Dictionary<IMyProgrammableBlock, Ini>();
+
+            private static readonly Ini Default = new Ini(true, true, true, true, true, true, true, 100f, 100f);
+
+            private readonly MyGridProgram program;
+
+            public static Ini FromBlock(MyGridProgram program, IMyProgrammableBlock block)
+            {
+                return FromBlockInternal(program, block, false);
+            }
+
+            public static Ini FromBlockOrSetDefault(MyGridProgram program, IMyProgrammableBlock block)
+            {
+                return FromBlockInternal(program, block, true);
+            }
+
+            private static Ini FromBlockInternal(MyGridProgram program, IMyProgrammableBlock block, bool setDefault)
+            {
+                string text;
+                if (IniTextCache.TryGetValue(block, out text) && text == block.CustomData)
+                {
+                    return IniCache[block];
+                }
+
+                IniTextCache[block] = block.CustomData;
+
+                Parser.Clear();
+                Ini ini = null;
+                if (Parser.TryParse(block.CustomData))
+                {
+                    if (Parser.ContainsSection(IniSection))
+                    {
+                        ini = new Ini(
+                            Parser.Get(IniSection, IniRechargeBatteries).ToBoolean(Default.RechargeBatteries),
+                            Parser.Get(IniSection, IniToggleGenerators).ToBoolean(Default.ToggleGenerators),
+                            Parser.Get(IniSection, IniStockpileHydrogen).ToBoolean(Default.StockpileHydrogen),
+                            Parser.Get(IniSection, IniToggleMotors).ToBoolean(Default.ToggleMotors),
+                            Parser.Get(IniSection, IniToggleThrusters).ToBoolean(Default.ToggleThrusters),
+                            Parser.Get(IniSection, IniToggleGyroscopes).ToBoolean(Default.ToggleGyroscopes),
+                            Parser.Get(IniSection, IniMaintainPower).ToBoolean(Default.MaintainPower),
+                            Parser.Get(IniSection, IniRechargeBatteriesOfConnectedShipsIfOverPercent)
+                                .ToSingle(Default.RechargeBatteriesOfConnectedShipsIfOverPercent),
+                            Parser.Get(IniSection, IniRefillHydrogenOfConnectedShipsIfOverPercent)
+                                .ToSingle(Default.RefillHydrogenOfConnectedShipsIfOverPercent)
+                        );
+                    }
+                }
+                else
+                {
+                    Parser.EndContent = block.CustomData;
+                }
+
+                IniCache[block] = ini;
+
+                if (!setDefault)
+                {
+                    return ini;
+                }
+
+                if (ini == null)
+                {
+                    ini = Default;
+                }
+
+                Parser.Set(IniSection, IniRechargeBatteries, ini.RechargeBatteries);
+                Parser.Set(IniSection, IniToggleGenerators, ini.ToggleGenerators);
+                Parser.Set(IniSection, IniStockpileHydrogen, ini.StockpileHydrogen);
+                Parser.Set(IniSection, IniToggleMotors, ini.ToggleMotors);
+                Parser.Set(IniSection, IniToggleThrusters, ini.ToggleThrusters);
+                Parser.Set(IniSection, IniToggleGyroscopes, ini.ToggleGyroscopes);
+                Parser.Set(IniSection, IniMaintainPower, ini.MaintainPower);
+                Parser.Set(IniSection, IniRechargeBatteriesOfConnectedShipsIfOverPercent,
+                    ini.RechargeBatteriesOfConnectedShipsIfOverPercent);
+                Parser.Set(IniSection, IniRefillHydrogenOfConnectedShipsIfOverPercent,
+                    ini.RefillHydrogenOfConnectedShipsIfOverPercent);
+                block.CustomData = Parser.ToString();
+
+                return ini;
+            }
+
+            public readonly bool RechargeBatteries;
+            public readonly bool ToggleGenerators;
+            public readonly bool StockpileHydrogen;
+            public readonly bool ToggleMotors;
+            public readonly bool ToggleThrusters;
+            public readonly bool ToggleGyroscopes;
+            public readonly bool MaintainPower;
+            public readonly float RechargeBatteriesOfConnectedShipsIfOverPercent;
+            public readonly float RefillHydrogenOfConnectedShipsIfOverPercent;
+
+            public Ini(bool rechargeBatteries, bool toggleGenerators, bool stockpileHydrogen, bool toggleMotors,
+                bool toggleThrusters, bool toggleGyroscopes, bool maintainPower,
+                float rechargeBatteriesOfConnectedShipsIfOverPercent, float refillHydrogenOfConnectedShipsIfOverPercent)
+            {
+                RechargeBatteries = rechargeBatteries;
+                ToggleGenerators = toggleGenerators;
+                StockpileHydrogen = stockpileHydrogen;
+                ToggleMotors = toggleMotors;
+                ToggleThrusters = toggleThrusters;
+                ToggleGyroscopes = toggleGyroscopes;
+                MaintainPower = maintainPower;
+                RechargeBatteriesOfConnectedShipsIfOverPercent = rechargeBatteriesOfConnectedShipsIfOverPercent;
+                RefillHydrogenOfConnectedShipsIfOverPercent = refillHydrogenOfConnectedShipsIfOverPercent;
+            }
+        }
+
         private readonly List<IMyCockpit> _cockpits = new List<IMyCockpit>();
         private readonly List<IMyRemoteControl> _remoteControls = new List<IMyRemoteControl>();
         private readonly List<IMyShipConnector> _connectors = new List<IMyShipConnector>();
@@ -41,13 +160,9 @@ namespace DockingManager
         private readonly List<IMyMotorSuspension> _motors = new List<IMyMotorSuspension>();
         private readonly List<IMyThrust> _thrusters = new List<IMyThrust>();
         private readonly List<IMyGyro> _gyroscopes = new List<IMyGyro>();
-        private bool _rechargeBatteries = true;
-        private bool _toggleGenerators = true;
-        private bool _stockpileHydrogen = true;
-        private bool _toggleMotors = true;
-        private bool _toggleThrusters = true;
-        private bool _toggleGyroscopes = true;
-        private bool _maintainPower = true;
+        private readonly List<IMyProgrammableBlock> _connectedDockingControllers = new List<IMyProgrammableBlock>();
+        private readonly List<IMyBatteryBlock> _connectedBatteries = new List<IMyBatteryBlock>();
+        private readonly List<IMyGasTank> _connectedHydrogenTanks = new List<IMyGasTank>();
 
         public Program()
         {
@@ -56,31 +171,8 @@ namespace DockingManager
 
         private void Main(string argument)
         {
-            _ini.Clear();
-            if (_ini.TryParse(Me.CustomData))
-            {
-                _rechargeBatteries = _ini.Get(IniSection, IniRechargeBatteries).ToBoolean(true);
-                _toggleGenerators = _ini.Get(IniSection, IniToggleGenerators).ToBoolean(true);
-                _stockpileHydrogen = _ini.Get(IniSection, IniStockpileHydrogen).ToBoolean(true);
-                _toggleMotors = _ini.Get(IniSection, IniToggleMotors).ToBoolean(true);
-                _toggleThrusters = _ini.Get(IniSection, IniToggleThrusters).ToBoolean(true);
-                _toggleGyroscopes = _ini.Get(IniSection, IniToggleGyroscopes).ToBoolean(true);
-                _maintainPower = _ini.Get(IniSection, IniMaintainPower).ToBoolean(true);
-            }
-            else if (!string.IsNullOrWhiteSpace(Me.CustomData))
-            {
-                _ini.EndContent = Me.CustomData;
-            }
+            var ini = Ini.FromBlockOrSetDefault(this, Me);
 
-            _ini.Set(IniSection, IniRechargeBatteries, _rechargeBatteries);
-            _ini.Set(IniSection, IniToggleGenerators, _toggleGenerators);
-            _ini.Set(IniSection, IniStockpileHydrogen, _stockpileHydrogen);
-            _ini.Set(IniSection, IniToggleMotors, _toggleMotors);
-            _ini.Set(IniSection, IniToggleThrusters, _toggleThrusters);
-            _ini.Set(IniSection, IniToggleGyroscopes, _toggleGyroscopes);
-            _ini.Set(IniSection, IniMaintainPower, _maintainPower);
-            Me.CustomData = _ini.ToString();
-            
             GridTerminalSystem.GetBlocksOfType(_cockpits, cockpit => cockpit.CubeGrid == Me.CubeGrid);
             GridTerminalSystem.GetBlocksOfType(_remoteControls, remoteControl => remoteControl.CubeGrid == Me.CubeGrid);
             GridTerminalSystem.GetBlocksOfType(_connectors, connector => connector.CubeGrid == Me.CubeGrid);
@@ -101,44 +193,102 @@ namespace DockingManager
 
             if (argument.Equals("undock", StringComparison.InvariantCultureIgnoreCase) || !connected)
             {
-                if (_rechargeBatteries)
+                if (ini.RechargeBatteries || ini.RechargeBatteriesOfConnectedShipsIfOverPercent < 100f)
                 {
                     _batteries.ForEach(battery => battery.ChargeMode = ChargeMode.Auto);
                 }
-                
-                if (_toggleGenerators)
+
+                if (ini.ToggleGenerators)
                 {
                     _generators.ForEach(generator => generator.Enabled = true);
                 }
-                
-                if (_stockpileHydrogen)
+
+                if (ini.StockpileHydrogen)
                 {
                     _hydrogenTanks.ForEach(tank => tank.Stockpile = false);
                 }
-                
-                if (_toggleMotors)
+
+                if (ini.ToggleMotors)
                 {
                     _motors.ForEach(motor => motor.Enabled = true);
                     _motors.ForEach(motor => motor.Brake = false);
                 }
-                
-                if (_toggleThrusters)
+
+                if (ini.ToggleThrusters)
                 {
                     _thrusters.ForEach(thruster => thruster.Enabled = true);
                 }
 
-                if (_toggleGyroscopes)
+                if (ini.ToggleGyroscopes)
                 {
                     _gyroscopes.ForEach(gyroscope => gyroscope.Enabled = true);
                 }
-                
+
                 _connectors.ForEach(connector => connector.Disconnect());
             }
             else
             {
-                if (_rechargeBatteries)
+                var rechargeConnectedBatteries = false;
+                if (ini.RechargeBatteriesOfConnectedShipsIfOverPercent < 100f)
                 {
-                    if (_batteries.Count == 1 && _maintainPower)
+                    if (_batteries.Select(battery => battery.CurrentStoredPower / battery.MaxStoredPower).Average() *
+                        100f > ini.RechargeBatteriesOfConnectedShipsIfOverPercent)
+                    {
+                        var directlyConnectedGrids = _connectors.Select(connector => connector.OtherConnector.CubeGrid);
+                        GridTerminalSystem.GetBlocksOfType(_connectedDockingControllers,
+                            block => directlyConnectedGrids.Any(grid => block.CubeGrid == grid) &&
+                                     Ini.FromBlock(this, block) != null);
+                        rechargeConnectedBatteries = _connectedDockingControllers.Any(controller =>
+                        {
+                            if (Ini.FromBlock(this, controller).RechargeBatteriesOfConnectedShipsIfOverPercent >
+                                ini.RechargeBatteriesOfConnectedShipsIfOverPercent)
+                            {
+                                GridTerminalSystem.GetBlocksOfType(_connectedBatteries,
+                                    battery => battery.CubeGrid == controller.CubeGrid);
+                                return _connectedBatteries.Any(battery =>
+                                    battery.CurrentStoredPower / battery.MaxStoredPower < 1f);
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        });
+                    }
+                }
+
+                var refillConnectedHydrogen = false;
+                if (ini.RefillHydrogenOfConnectedShipsIfOverPercent < 100f)
+                {
+                    if (_hydrogenTanks.Select(tank => tank.FilledRatio).Average() * 100f >
+                        ini.RefillHydrogenOfConnectedShipsIfOverPercent)
+                    {
+                        GridTerminalSystem.GetBlocksOfType(_connectedDockingControllers,
+                            block => block.CubeGrid != Me.CubeGrid && Ini.FromBlock(this, block) != null);
+                        refillConnectedHydrogen = _connectedDockingControllers.Any(controller =>
+                        {
+                            if (Ini.FromBlock(this, controller).RefillHydrogenOfConnectedShipsIfOverPercent >
+                                ini.RefillHydrogenOfConnectedShipsIfOverPercent)
+                            {
+                                GridTerminalSystem.GetBlocksOfType(_connectedHydrogenTanks,
+                                    tank => tank.CubeGrid == controller.CubeGrid &&
+                                            tank.BlockDefinition.SubtypeId.Contains("Hydrogen"));
+                                return _connectedHydrogenTanks.Any(tank => tank.FilledRatio < 1d);
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        });
+                    }
+                }
+
+                if (rechargeConnectedBatteries)
+                {
+                    _batteries.ForEach(battery => battery.ChargeMode = ChargeMode.Discharge);
+                }
+                else if (ini.RechargeBatteries)
+                {
+                    if (_batteries.Count == 1 && ini.MaintainPower)
                     {
                         _batteries.ForEach(battery =>
                         {
@@ -154,7 +304,7 @@ namespace DockingManager
                     }
                     else
                     {
-                        var backupBattery = _maintainPower
+                        var backupBattery = ini.MaintainPower
                             ? _batteries.MaxBy(battery => battery.CurrentStoredPower)
                             : null;
 
@@ -163,28 +313,32 @@ namespace DockingManager
                             : ChargeMode.Recharge);
                     }
                 }
-                
-                if (_toggleGenerators)
+
+                if (ini.ToggleGenerators)
                 {
                     _generators.ForEach(generator => generator.Enabled = false);
                 }
-                
-                if (_stockpileHydrogen)
+
+                if (refillConnectedHydrogen)
+                {
+                    _hydrogenTanks.ForEach(tank => tank.Stockpile = false);
+                }
+                else if (ini.StockpileHydrogen)
                 {
                     _hydrogenTanks.ForEach(tank => tank.Stockpile = true);
                 }
-                
-                if (_toggleMotors)
+
+                if (ini.ToggleMotors)
                 {
                     _motors.ForEach(motor => motor.Enabled = false);
                 }
-                
-                if (_toggleThrusters)
+
+                if (ini.ToggleThrusters)
                 {
                     _thrusters.ForEach(thruster => thruster.Enabled = false);
                 }
 
-                if (_toggleGyroscopes)
+                if (ini.ToggleGyroscopes)
                 {
                     _gyroscopes.ForEach(gyroscope => gyroscope.Enabled = false);
                 }
